3a43b8eeaa340325e7963c221ede291b7201469a
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-10-25 15:00:02 +0200

    Basic rendering of mobile menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,30 @@ import data from "./portfolio_data.js";
 import errors from "./errors.js";
 import Message from "./utilities.js";
 
-console.dir(data);
+const render = function () {
+  renderHeader();
+};
+
+const renderHeader = function () {
+  renderLogo();
+  renderMenu();
+};
+
+const renderLogo = function () {
+  const logo = document.querySelector(".header-logo");
+  logo.innerText = data.header.logo;
+};
+
+const renderMenu = function () {
+  const menuDiv = document.querySelector(".header-mobile-menu");
+  const menuList = document.createElement("ul");
+  menuDiv.appendChild(menuList);
+  const links = data.links;
+  for (const link of links) {
+    const item = document.createElement("li");
+    item.innerText = link;
+    menuList.appendChild(item);
+  }
+};
+
+render();
