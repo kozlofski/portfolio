74ab917a36cead1e4bcacd75dd88e7f0e58a3d9b
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-10-29 10:46:36 +0100

    Added font_awesome.js for storing font awesome snippets

---
+++ scripts/font_awesome.js
@@ -0,0 +1,8 @@
+const fa = {
+  leftArrow: '<i class="fa-solid fa-arrow-left"></i>',
+  rightArrow: '<i class="fa-solid fa-arrow-right"></i>',
+  plus: '<i class="fa-solid fa-plus"></i>',
+  copyright: '<i class="fa-regular fa-copyright"></i>',
+};
+
+export default fa;
