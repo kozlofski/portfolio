729294c95d8d7632403234d8059028653b0ba6c6
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-11-03 14:28:50 +0100

    Trashbin icon added

--- scripts/font_awesome.js
+++ scripts/font_awesome.js
@@ -4,6 +4,7 @@ const fa = {
   plus: '<i class="fa-solid fa-plus"></i>',
   copyright: '<i class="fa-regular fa-copyright"></i>',
   xmark: '<i class="fa-solid fa-xmark"></i>',
+  trashbin: '<i class="fa-solid fa-trash-can"></i>',
 };
 
 export default fa;
