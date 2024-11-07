8c8f9388ddd657ce7ebc69a73479879f2bea0eb1
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-11-07 07:48:51 +0100

    Restored left and right arrow in font_awesome.js

--- scripts/font_awesome.js
+++ scripts/font_awesome.js
@@ -1,6 +1,8 @@
 const fa = {
   upArrow: '<i class="fa-solid fa-arrow-up"></i>',
   downArrow: '<i class="fa-solid fa-arrow-down"></i>',
+  leftArrow: '<i class="fa-solid fa-arrow-left"></i>',
+  rightArrow: '<i class="fa-solid fa-arrow-right"></i>',
   plus: '<i class="fa-solid fa-plus"></i>',
   copyright: '<i class="fa-regular fa-copyright"></i>',
   xmark: '<i class="fa-solid fa-xmark"></i>',
